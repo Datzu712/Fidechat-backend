e3de5008594f9ac0cd6cf0b39019faba8d058c50
Datzu712 <devdatzucl@gmail.com>
2024-12-01 16:30:23 -0600

    Added abstract event type

---
+++ src/main/java/com/fidechat/entities/Event.java
@@ -0,0 +1,19 @@
+package com.fidechat.entities;
+
+public class Event<T> {
+    private EventsEnum type;
+    private T payload;
+
+    public Event(EventsEnum type, T payload) {
+        this.type = type;
+        this.payload = payload;
+    }
+
+    public EventsEnum getType() {
+        return this.type;
+    }
+
+    public T getPayload() {
+        return this.payload;
+    }
+}
---
+++ src/main/java/com/fidechat/entities/EventsEnum.java
@@ -0,0 +1,6 @@
+package com.fidechat.entities;
+
+public enum EventsEnum {
+    MESSAGE_CREATE,
+    MEMBER_JOIN
+}
